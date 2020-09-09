c09f96c49ae7fb17761d4a2e3713a124d5e63eb6
Ondrej Kuchta <xkucht04@gmail.com>
2020-09-09 11:27:06 +0200

    added function to the anonymization helper

--- src/helpers/anonymization.helper.ts
+++ src/helpers/anonymization.helper.ts
@@ -1,5 +1,6 @@
 import * as emailAddresses from 'email-addresses'
 import sha512 from './sha512'
+import rsa from './rsa'
 import config from '../config'
 
 export type AnonymizeEmailConfig = {
@@ -13,6 +14,10 @@ export type AnonymizeEmailConfig = {
 
 const hash = (str: string, salt: string, length = 16) => {
   return sha512(salt, str).substr(0, length)
+}
+
+const decrypt = (str: string, privateKey: string) => {
+  return rsa.rsa_decrypt(str, privateKey)
 }
 
 const getConfig = (params?: AnonymizeEmailConfig): AnonymizeEmailConfig => {
