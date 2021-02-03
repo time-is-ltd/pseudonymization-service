821db9f1fded18ec552805a07baec95ead1d0337
Ondrej Kuchta <ondrej@Ondrejs-MacBook-Air-2.local>
2021-02-03 23:48:55 +0100

    KV Azure

--- src/modules/googleapis/googleapis.config.ts
+++ src/modules/googleapis/googleapis.config.ts
@@ -2,10 +2,8 @@ import { getPathPartFactory } from '../../helpers/path.helper'
 import { toArray, toPem } from '../../helpers/config.helper'
 
 // Env variables
-export const clientEmail: string | undefined = 
-process.env.GSUITE_CLIENT_EMAIL
-export const privateKey: string = 
-toPem(process.env.GSUITE_PRIVATE_KEY || '')
+export const clientEmail: string | undefined = process.env.GSUITE_CLIENT_EMAIL
+export const privateKey: string = toPem(process.env.GSUITE_PRIVATE_KEY || '')
 export const scopes: string[] = toArray(process.env.GSUITE_SCOPES)
 
 // Hosts
