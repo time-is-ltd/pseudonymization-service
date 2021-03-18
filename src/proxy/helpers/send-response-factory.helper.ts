acc91283ca52d5fe0657b2d9ebc9d51e6b2f4a3f
janjilek <jilekj@gmail.com>
2021-03-18 14:58:48 +0100

    feat(proxy): send whitelisted http headers only (#22)

--- src/proxy/helpers/send-response-factory.helper.ts
+++ src/proxy/helpers/send-response-factory.helper.ts
@@ -4,31 +4,35 @@ interface ResponseOptions {
   statusCode?: number
   statusMessage?: string
   headers?: OutgoingHttpHeaders
+  allowedHeaders?: string[]
   data?: string
 }
 
 export const sendResponseFactory = (response: ServerResponse) => (options: ResponseOptions = {}) => {
-  const { statusCode = 500, statusMessage, headers = {}, data = '{}' } = options
+  const { statusCode = 500, statusMessage, headers = {}, allowedHeaders = [], data = '{}' } = options
   const contentLength = Buffer.byteLength(data)
 
-  // Modify headers
-  // Remove content encoding
-  delete headers['content-encoding']
+  // Add allowed incoming headers to the outgoing headers
+  const outgoingHeaders: OutgoingHttpHeaders = allowedHeaders
+    .reduce((obj, headerName) => {
+      if (headers[headerName]) {
+        obj[headerName] = headers[headerName]
+      }
+      return obj
+    }, {})
 
   // Set content length
-  headers['content-length'] = contentLength
+  outgoingHeaders['content-length'] = contentLength
 
   // Set content type
-  if (!headers['content-type']) {
-    headers['content-type'] = 'application/json'
-  }
+  outgoingHeaders['content-type'] = headers['content-type'] ?? 'application/json'
 
   // This fixes bug in writeHead function
   // if statusMessage is null, headers are not send
   if (statusMessage) {
-    response.writeHead(statusCode, statusMessage, headers)
+    response.writeHead(statusCode, statusMessage, outgoingHeaders)
   } else {
-    response.writeHead(statusCode, headers)
+    response.writeHead(statusCode, outgoingHeaders)
   }
 
   response.write(data)
