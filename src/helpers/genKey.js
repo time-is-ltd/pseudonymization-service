978a39ad937f9d787da3f7343341ae736100c3f2
Jan Jílek <jilekj@gmail.com>
2021-03-19 18:27:12 +0100

    fix: add genKey.js helper

---
+++ src/helpers/genKey.js
@@ -0,0 +1,27 @@
+// This utility generates one-line PEM format Private and Public Keys
+const crypto = require('crypto')
+
+const generateKey = () => {
+  crypto.generateKeyPair('rsa', {
+    modulusLength: 4096,
+    publicKeyEncoding: {
+      type: 'spki',
+      format: 'pem'
+    },
+    privateKeyEncoding: {
+      type: 'pkcs8',
+      format: 'pem',
+    }
+  }, (err, publicKey, privateKey) => {
+    // Handle errors and use the generated key pair.
+    console.log('\n')
+    const oneLinePrivateKey = privateKey.replace(/\r/g, '').replace(/\n/g, '\\n')
+    console.log(oneLinePrivateKey)
+
+    console.log('\n')
+    const oneLinePublicKey = publicKey.replace(/\r/g, '').replace(/\n/g, '\\n')
+    console.log(oneLinePublicKey)
+  })
+}
+
+generateKey()
