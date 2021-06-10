fa15427854e3b5e60dcc02e860c428cdc7a33db0
Ondrej Ilcik <ondrej.ilcik@timeisltd.com>
2021-06-10 15:01:36 +0200

    refactor(all): add linter config + fix auto-fixable issues

--- src/router/interfaces/router.interface.ts
+++ src/router/interfaces/router.interface.ts
@@ -2,7 +2,7 @@ import { RequestHandler } from 'express'
 
 export interface Route {
   hosts: string[],
-  path: string,
+  path: string|string[],
   handler: RequestHandler | RequestHandler[],
   requireAuth?: boolean,
   method?: 'get' | 'post' | 'put' | 'patch'
